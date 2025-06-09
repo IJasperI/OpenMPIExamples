a4e2a53e88723d6d1465553fd61a246ea6659ea4
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2025-06-09 14:22:51 +0200

    Working communication MPI_Send and MPI_Recv example updated readme

--- mpi_comm_test.c
+++ mpi_comm_test.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+// ignore error here
 #include <mpi.h>
 
 int main(){
-    MPI_INIT(NULL, NULL);
+    int numtask, rank, dest, source, rc, count, tag = 1;
+    char inmsg, outmsg = 'x';
+    MPI_Status stat;
 
-    MPI_FINALIZE();
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtask);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0){
+        dest = 1;
+        source = 1;
+        outmsg = 'a';
+        MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &stat);
+    } else if (rank == 1){
+        dest = 0;
+        source = 0;
+        outmsg = 'b';
+        MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &stat);
+        MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    }
+    MPI_Get_count(&stat, MPI_CHAR, &count);
+    if (rank == 0 || rank == 1){
+        printf("Task %d: Received %d char(s) from task %d with tag %d \nThe char was: %c \n",
+            rank, count, stat.MPI_SOURCE, stat.MPI_TAG, inmsg);
+    }
+
+
+    MPI_Finalize();
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # How to
 Run the job.sh script to run the code.<br>
+To switch to a diffrent file change up job.sh.<br>
 If u want to run files by yourself, use the <b>mpicc</b> command instead of gcc<br>
